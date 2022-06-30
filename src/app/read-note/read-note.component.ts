88cdd3104c09436523cf19fbe9a09a29cd7c0dfc
imohammadsadra <mohammadsadrahaeri@gmail.com>
2022-06-30 17:21:52 +0430

    Added all pages

--- src/app/read-note/read-note.component.ts
+++ src/app/read-note/read-note.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../services/data.service';
+import {RequestService} from '../services/request.service';
 
 @Component({
   selector: 'app-read-note',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadNoteComponent implements OnInit {
 
-  constructor() { }
+  linkId;
+  loading = true;
+  body;
+  status;
+  constructor(private dataService: DataService,
+              private requestService: RequestService) { }
 
   ngOnInit(): void {
+    this.dataService.currentLinkSource.subscribe(link => {
+      if (link) {
+        this.linkId = link;
+        this.read();
+      }
+    });
   }
+
+  private read() {
+    this.loading = true;
+    this.requestService.read(this.linkId).subscribe(res => {
+      if (res['status'] === 1){
+        this.status = 1;
+      }
+     else if(res['status'] === 2){
+        this.body = res['noteBody'];
+        this.status = 2;
+      }
+      this.loading = false;
+      // this.dataService.changeCurrentLink(res['link'] as string);
+    });
+  }
+
 
 }
--- src/app/request-read/request-read.component.ts
+++ src/app/request-read/request-read.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {RequestService} from '../services/request.service';
+import {DataService} from '../services/data.service';
 
 @Component({
   selector: 'app-request-read',
@@ -8,11 +10,46 @@ import {Router} from '@angular/router';
 })
 export class RequestReadComponent implements OnInit {
 
-  constructor(private router: Router) {
-    console.log(router.url);
+  linkId = '';
+  status;
+  loading = true;
+
+  constructor(private router: Router,
+              private requestService: RequestService,
+              private dataService: DataService) {
+    console.log(router.url.split('/')[2]);
+    this.linkId = router.url.split('/')[2];
   }
 
   ngOnInit(): void {
+    this.checkIsAvailable();
+  }
+
+  // tslint:disable-next-line:typedef
+  private checkIsAvailable() {
+    this.loading = true;
+    this.requestService.requestRead(this.linkId).subscribe(res => {
+      if (res['status'] === 0) {
+        this.status = 0;
+      } else if (res['status'] === 1) {
+        this.status = 1;
+      } else if (res['status'] === 2) {
+        this.status = 2;
+      }
+      this.loading = false;
+      // this.dataService.changeCurrentLink(res['link'] as string);
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  goToReadPage() {
+    this.dataService.changeCurrentLink(this.linkId as string);
+    this.router.navigate(['read']);
+  }
+
+  // tslint:disable-next-line:typedef
+  backHome() {
+    this.router.navigate(['create']);
   }
 
 }
